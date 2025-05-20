155de96e112a6beed72c6083a0e0ddb2763edf9e
rabidrancor <118483554+RabidRancor@users.noreply.github.com>
2025-05-20 01:23:16 -0400

    Edited the docx parsing function to allow arbitrary amount of (similarly formatted) docx files as input

--- document_processing.py
+++ document_processing.py
@@ -1,88 +1,89 @@
 from docx import Document
-from json import dump
+import json
 
 
 
-def word_to_list(filename: str):
+def word_to_list(files: list):
 
-    '''Extracts content from the docx file (filename) and converts it to a list of dictionaries'''
+    '''Extracts content from arbitrary amount of docx files and converts it to a list of dictionaries'''
 
     finaloutput = []
 
-    current_word = ""
-    current_examples = []
+    #file = file name from a list of file names
+    for file in files:
 
 
-    #our file read object
-    doc = Document(filename)
+        #our file read object
+        doc = Document(file)
 
 
-    #each paragraph in the document
-    for paragraph in doc.paragraphs:
-        
-        #extract the paragraph as a string with newline/carriage return removed
-        text = paragraph.text.strip()
-
-        #empty paragraph is ignored
-        if not text:
-            continue
-
-
-        bold = False
-
-
-        #check the runs in this paragraph for any bold
-        #if it detects a bold run, immediately sets the bold flag to True
-        #and stops searching the paragraph
-
-        for run in paragraph.runs:
-            if run.bold:
-                bold = True
-                break
+        current_word = ""
+        current_examples = []
 
 
 
-        #if the paragraph is bold text
-        if bold:
-
-
-            #if the current line is a bold one
-            #and there is a value already in current_word
-            #then that means you're onto the next word and should upload
-            #the previous word and examples to the list
-
-            #this is only triggered when you reach the second word and onwards
-
-            if current_word:
-
-                finaloutput.append({'word': current_word, 'examples': current_examples})
-
+        #each paragraph in the document
+        for paragraph in doc.paragraphs:
             
+            #extract the paragraph as a string with newline/carriage return removed
+            text = paragraph.text.strip()
 
-            #currentword is set whenever a bold paragraph is detected
-            #and current_examples is cleared
-            current_word = text
-            current_examples = []
-            
-
-            
-        #if the current paragraph is not bold
-        #then it is an example for current_word
-        #and should be appended to the list
-
-        else:
-            current_examples.append(text)
+            #empty paragraph is ignored
+            if not text:
+                continue
 
 
 
-
-    #when the above code reaches the last word, it will fill current_word and current_examples
-    #but wont be able to trigger that nested if statement for current_word
-    #so the for loop will end but current_word and current_examples retain their contents
-    #since they're global variables
+            #check the runs in this paragraph for any evidence of bolding
+            #bold is a boolean flag
+            #if it detects any bolded runs, it outputs a True
 
 
-    finaloutput.append({'word': current_word, 'examples': current_examples})
+            bold = any(run.bold for run in paragraph.runs)
+
+
+
+            #if the paragraph is bold text
+            if bold:
+
+
+                #if the current line is a bold one
+                #and there is a value already in current_word
+                #then that means you're onto the next word and should upload
+                #the previous word and examples to the list
+
+                #this is only triggered when you reach the second word and onwards
+
+                if current_word:
+
+                    finaloutput.append({'word': current_word, 'examples': current_examples})
+
+                
+
+                #currentword is set whenever a bold paragraph is detected
+                #and current_examples is cleared
+                current_word = text
+                current_examples = []
+                
+
+                
+            #if the current paragraph is not bold
+            #then it is an example for current_word
+            #and should be appended to the list
+
+            else:
+                current_examples.append(text)
+
+
+
+
+        #when the above code reaches the last word, it will fill current_word and current_examples
+        #but wont be able to trigger that nested if statement for current_word
+        #so the for loop will end but current_word and current_examples retain their contents
+        #since they're global variables
+
+
+        finaloutput.append({'word': current_word, 'examples': current_examples})
 
     return finaloutput
 
@@ -90,17 +91,20 @@ def word_to_list(filename: str):
 
 
 
-def list_to_json(vocablist: list, outputfile: str = 'vocab.json'):
+def list_to_json(vocablist: list, filename: str = 'vocab.json'):
+
+    '''creates a json file populated with contents of a list of dictionaries'''
+
 
     #utf-8 is cross compatible
 
-    with open(outputfile, 'w', encoding = 'utf-8') as j:
+    with open(filename, 'w', encoding = 'utf-8') as j:
 
 
-        #ensure ascii can help with accented or special unicode characters
+        #ensure_ascii can help with accented or special unicode characters
         #indentation used so the .json file is readable
 
-        dump(vocablist, j, ensure_ascii = False, indent = 2)
+        json.dump(vocablist, j, ensure_ascii = False, indent = 2)
 
 
 
@@ -108,6 +112,10 @@ def list_to_json(vocablist: list, outputfile: str = 'vocab.json'):
 
 
 if __name__ == '__main__':
-    filename = input('Which file to access: ')
     
-    list_to_json(word_to_list(filename))
+    files = []
+    while (userchoice := input("Add .docx files to convert to .json (blank to stop inputting): ")):
+        files.append(userchoice)
+    
+
+    list_to_json(word_to_list(files))
